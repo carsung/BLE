8db56eff826107d486aa5a9b5de61b45245bfc83
shcha <shcha@amtel.co.kr>
2022-09-03 23:21:02 +0900

    remove unused callbacks

--- app/src/main/java/org/gbsa/lecture/blesensormonitor/BluetoothLeService.java
+++ app/src/main/java/org/gbsa/lecture/blesensormonitor/BluetoothLeService.java
@@ -54,16 +54,6 @@ public class BluetoothLeService extends Service {
 
    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
        @Override
-       public void onPhyUpdate(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-           super.onPhyUpdate(gatt, txPhy, rxPhy, status);
-       }
-
-       @Override
-       public void onPhyRead(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-           super.onPhyRead(gatt, txPhy, rxPhy, status);
-       }
-
-       @Override
        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
            String intentAction;
            if (newState == BluetoothProfile.STATE_CONNECTED) {
@@ -106,36 +96,6 @@ public class BluetoothLeService extends Service {
        @Override
        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
            broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
-       }
-
-       @Override
-       public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-           super.onDescriptorRead(gatt, descriptor, status);
-       }
-
-       @Override
-       public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-           super.onDescriptorWrite(gatt, descriptor, status);
-       }
-
-       @Override
-       public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
-           super.onReliableWriteCompleted(gatt, status);
-       }
-
-       @Override
-       public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
-           super.onReadRemoteRssi(gatt, rssi, status);
-       }
-
-       @Override
-       public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
-           super.onMtuChanged(gatt, mtu, status);
-       }
-
-       @Override
-       public void onServiceChanged(@NonNull BluetoothGatt gatt) {
-           super.onServiceChanged(gatt);
        }
    };
 
